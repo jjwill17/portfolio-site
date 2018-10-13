e65233f0aa869b447c99fe47879fb16fddfe4c63
Justin <justinjwillmore@gmail.com>
2018-10-13 12:31:16 -0700

    slight wording changes for About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className='about'>
             <h1 className='page-headers'>About Me</h1>
-            <p>Ever since I found my passion for coding, I haven't been able to quench the thirst I have for learning more. The first programming language that I started with was C++ and it was then that I decided that coding was one of the coolest things ever created. I have since put a lot of focus in JavaScript, while also looking into other programming languages and their awesome potential.</p>
+            <p>Ever since I found my passion for coding, I haven't been able to quench the thirst for learning more. The first programming language I started with was C++ and it was then that I decided programming was one of the coolest things ever. I have since put a lot of focus in JavaScript, while also looking into other programming languages and their awesome potential.</p>
         </div>
     );
 };
